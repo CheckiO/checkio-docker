e95bd2a663d11413746200e77cf4498acb6a007d
Lubimov Igor <infunt@gmail.com>
2015-02-20 14:32:04 +0200

    renamed client class

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='checkio_docker',
-    version='0.0.1',
+    version='0.0.2',
     description='Build docker images from CheckiO mission module',
     author='CheckiO',
     author_email='igor@checkio.org',
     url='https://github.com/CheckiO/checkio-docker',
-    download_url='https://github.com/CheckiO/checkio-docker/tarball/0.1',
+    download_url='https://github.com/CheckiO/checkio-docker/tarball/0.2',
     packages=find_packages(),
 )
