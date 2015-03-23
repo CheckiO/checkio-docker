d71c2858a4248ad08f6a7437cd1f13725bcf4c88
Lubimov Igor <infunt@gmail.com>
2015-03-23 14:07:14 +0200

    added multienvs parser

--- checkio_docker/parser.py
+++ checkio_docker/parser.py
@@ -9,30 +9,46 @@ class MissionFilesException(Exception):
     pass
 
 
-class MissionFilesHandler(object):
+class MissionFilesCompiler(object):
 
     DIR_VERIFICATION = 'verification'
+    DIR_VERIFICATION_ENVS = 'envs'
+    DIR_INITIAL_CODES = 'initial'
 
     SCHEMA_FILENAME = 'schema'
 
     DOCKER_MAIN_FILENAME = 'Dockertemplate'
     DOCKER_ENV_FILENAME = 'Dockerenv'
 
-    RUNNER_FILENAME = 'run.sh'
 
-    def __init__(self, env, working_path):
-        self._env = env
-        self.working_path = os.path.join(working_path, 'checkio_mission')
+    def __init__(self, working_path):
+        self.working_path = working_path
         self.path_verification = os.path.join(self.working_path, self.DIR_VERIFICATION)
-        self.path_runner = os.path.join(self.path_verification, 'envs', self.RUNNER_FILENAME)
+        self.path_envs = os.path.join(self.path_verification, self.DIR_VERIFICATION_ENVS)
+        self.path_initial = os.path.join(self.working_path, self.DIR_INITIAL_CODES)
 
     def compile_from_files(self, source_path):
-        repository = self.get_base_repository(source_path)
-        self.git_pull(repository, self.working_path)
+        base_repositories = self.download_base_repositories(source_path)
+        if base_repositories:
+            base_repositories.reverse()
+            for repository_path in base_repositories:
+                copy_tree(repository_path, self.working_path)
+                shutil.rmtree(repository_path)
         self.copy_user_files(source_path)
-        self.make_env_runner()
-        self.make_dockerfile()
+        active_envs = self.get_active_envs()
+        self.filter_envs(active_envs)
+        self.make_dockerfile(active_envs)
         return self.path_verification
+
+    def download_base_repositories(self, source_path):
+        repository = self.get_base_repository(source_path)
+        base_repositories = []
+        while repository is not None:
+            base_repository_path = tempfile.mkdtemp()
+            self.git_pull(repository, base_repository_path)
+            base_repositories.append(base_repository_path)
+            repository = self.get_base_repository(base_repository_path)
+        return base_repositories
 
     def compile_from_git(self, repository):
         source_path = None
@@ -52,7 +68,7 @@ class MissionFilesHandler(object):
         """
         schema_file = os.path.join(source_path, self.SCHEMA_FILENAME)
         if not os.path.exists(schema_file):
-            raise MissionFilesException('Schema file does not exists.')
+            return None
 
         with open(schema_file, 'r') as f:
             content = f.readline()
@@ -87,23 +103,24 @@ class MissionFilesHandler(object):
     def copy_user_files(self, source_path):
         copy_tree(source_path, self.working_path)
 
-    def make_env_runner(self):
-        runner_template = self._get_file_content(self.path_runner)
-        runner_template = runner_template.replace('{{env}}', self._env)
-        with open(self.path_runner, 'w') as f:
-            f.write(runner_template)
+    def get_active_envs(self):
+        return os.listdir(self.path_initial)
 
-    def make_dockerfile(self):
+    def filter_envs(self, active_envs):
+        for env in os.listdir(self.path_envs):
+            if env not in active_envs:
+                shutil.rmtree(os.path.join(self.path_envs, env))
+
+    def make_dockerfile(self, active_envs):
+        envs_docker = []
+        for env in active_envs:
+            docker_env_file = os.path.join(self.path_verification, 'envs', env, self.DOCKER_ENV_FILENAME)
+            envs_docker.append(self._get_file_content(docker_env_file))
+
         docker_main_file = os.path.join(self.path_verification, self.DOCKER_MAIN_FILENAME)
-        docker_env_file = os.path.join(self.path_verification, 'envs', self._env,
-                                       self.DOCKER_ENV_FILENAME)
-
         docker_main = self._get_file_content(docker_main_file)
-        docker_env = self._get_file_content(docker_env_file)
 
-        docker_main = docker_main.replace('{{env_instructions}}', docker_env)
-        docker_main = docker_main.replace('{{env}}', self._env)
-
+        docker_main = docker_main.replace('{{env_instructions}}', "\n".join(envs_docker))
         dockerfile_path = os.path.join(self.path_verification, 'Dockerfile')
         with open(dockerfile_path, 'w') as f:
             f.write(docker_main)
